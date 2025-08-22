c73a8ed4d8fc16b131188c5afdacd376bf1a5360
Mahrukh1612 <mahrukhf520@gmail.com>
2025-08-22 14:23:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const contactform = document.getElementById('ContactForm');
+  contactform.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    alert("Your message is sent to our email.")
+  })
+
+  const Formgroup = document.getElementById('form-group');
+  Formgroup.addEventListener((click), (e)=>{
+ e.preventDefault();
+    
+  })
